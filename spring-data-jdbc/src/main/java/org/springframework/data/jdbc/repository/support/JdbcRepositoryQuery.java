618d271df1feeb500db405212c59ae40e74827fc
guodage <guodage003@gmail.com>
2020-06-07 17:18:14 +0800

    extend to support Page<T>

--- spring-data-jdbc/src/main/java/org/springframework/data/jdbc/repository/support/JdbcRepositoryQuery.java
+++ spring-data-jdbc/src/main/java/org/springframework/data/jdbc/repository/support/JdbcRepositoryQuery.java
@@ -15,27 +15,33 @@
  */
 package org.springframework.data.jdbc.repository.support;
 
-import java.lang.reflect.Constructor;
-import java.util.List;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mapping.callback.EntityCallbacks;
 import org.springframework.data.relational.core.mapping.RelationalMappingContext;
 import org.springframework.data.relational.core.mapping.RelationalPersistentEntity;
 import org.springframework.data.relational.core.mapping.event.AfterLoadCallback;
 import org.springframework.data.relational.core.mapping.event.AfterLoadEvent;
 import org.springframework.data.relational.core.mapping.event.Identifier;
+import org.springframework.data.repository.query.Parameters;
 import org.springframework.data.repository.query.RepositoryQuery;
+import org.springframework.data.repository.support.PageableExecutionUtils;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
+
+import java.lang.reflect.Constructor;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A query to be executed based on a repository method, it's annotated SQL query and the arguments provided to the
@@ -108,6 +114,13 @@ class JdbcRepositoryQuery implements RepositoryQuery {
 					: createListRowMapperQueryExecutor(query, rowMapper);
 			return createCollectionQueryExecutor(innerExecutor);
 		}
+		if (queryMethod.isPageQuery()){
+			QueryExecutor<Object> innerExecutor = extractor != null ? createResultSetExtractorQueryExecutor(query, extractor)
+					: createListRowMapperQueryExecutor(query, rowMapper);
+			Parameters<?, ?> parametersInQueryMethod = queryMethod.getParameters();
+			String pageableParamName = parametersInQueryMethod.getParameter(parametersInQueryMethod.getPageableIndex()).getName().get();
+			return createPageQueryExecutor(query, pageableParamName, createCollectionQueryExecutor(innerExecutor));
+		}
 
 		QueryExecutor<Object> innerExecutor = extractor != null ? createResultSetExtractorQueryExecutor(query, extractor)
 				: createObjectRowMapperQueryExecutor(query, rowMapper);
@@ -158,6 +171,35 @@ class JdbcRepositoryQuery implements RepositoryQuery {
 		};
 	}
 
+	private QueryExecutor<Object> createPageQueryExecutor(String querySql, String pageableParamName, QueryExecutor<Object> executor) {
+		return parameters -> {
+			Pageable pageable = (Pageable) parameters.getValue(pageableParamName);
+			parameters.addValue("limit", pageable.getPageSize());
+			parameters.addValue("offset", pageable.getOffset());
+			List<?> result = (List<?>) executor.execute(parameters);
+
+			return PageableExecutionUtils.getPage(
+					result,
+					pageable,
+					() -> {
+						String countQuerySql = querySql.replaceFirst("(?i)select .*? from", "select count(*) from")
+								.replaceFirst("(?i) order by .*", "");
+						return operations.queryForObject(countQuerySql, parameters, SingleColumnRowMapper.newInstance(Long.class));
+					}
+			);
+		};
+	}
+
+	private String extractOrderSql(Sort sort) {
+		//format like : a desc, b asc
+		Assert.isTrue(!sort.isEmpty(), "need pagination but no order?");
+		//todo only support first property half-dynamically
+		List<String> subOrderSqlList = sort.toList().stream().map(
+				item -> item.getProperty()
+		).collect(Collectors.toList());
+		return String.join(",", subOrderSqlList);
+	}
+
 	private QueryExecutor<Object> createModifyingQueryExecutor(String query) {
 
 		return parameters -> {
@@ -200,18 +242,32 @@ class JdbcRepositoryQuery implements RepositoryQuery {
 			throw new IllegalStateException(String.format("No query specified on %s", queryMethod.getName()));
 		}
 
+		if (queryMethod.isPageQuery()) {
+			query = enhancePageQuery(query);
+		}
+
 		return query;
+	}
+
+	private String enhancePageQuery(String query) {
+		String original = query.trim().replace(";", "");
+		return String.format("%s limit :limit offset :offset", original);
 	}
 
 	private MapSqlParameterSource bindParameters(Object[] objects) {
 
 		MapSqlParameterSource parameters = new MapSqlParameterSource();
 
-		queryMethod.getParameters().getBindableParameters().forEach(p -> {
-
+		Parameters<?, ?> parametersInQueryMethod = queryMethod.getParameters();
+		parametersInQueryMethod.getBindableParameters().forEach(p -> {
 			String parameterName = p.getName().orElseThrow(() -> new IllegalStateException(PARAMETER_NEEDS_TO_BE_NAMED));
 			parameters.addValue(parameterName, objects[p.getIndex()]);
 		});
+
+		if (queryMethod.isPageQuery()) {
+			String pageableParamName = parametersInQueryMethod.getParameter(parametersInQueryMethod.getPageableIndex()).getName().get();
+			parameters.addValue(pageableParamName, objects[parametersInQueryMethod.getPageableIndex()]);
+		}
 
 		return parameters;
 	}
