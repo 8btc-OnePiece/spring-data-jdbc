8be1023815bf9b0a37e83b93baa51333a1ef9642
guodage <guodage003@gmail.com>
2023-02-24 20:38:30 +0800

    remove not support, add SNAPSHOT version.

--- spring-data-jdbc/src/main/java/org/springframework/data/jdbc/repository/query/StringBasedJdbcQuery.java
+++ spring-data-jdbc/src/main/java/org/springframework/data/jdbc/repository/query/StringBasedJdbcQuery.java
@@ -74,15 +74,16 @@ public class StringBasedJdbcQuery extends AbstractJdbcQuery {
 		this.queryMethod = queryMethod;
 		this.converter = converter;
 
-		if (queryMethod.isSliceQuery()) {
-			throw new UnsupportedOperationException(
-					"Slice queries are not supported using string-based queries. Offending method: " + queryMethod);
-		}
-
-		if (queryMethod.isPageQuery()) {
-			throw new UnsupportedOperationException(
-					"Page queries are not supported using string-based queries. Offending method: " + queryMethod);
-		}
+		//try to impl this
+//		if (queryMethod.isSliceQuery()) {
+//			throw new UnsupportedOperationException(
+//					"Slice queries are not supported using string-based queries. Offending method: " + queryMethod);
+//		}
+//
+//		if (queryMethod.isPageQuery()) {
+//			throw new UnsupportedOperationException(
+//					"Page queries are not supported using string-based queries. Offending method: " + queryMethod);
+//		}
 
 		executor = Lazy.of(() -> {
 			RowMapper<Object> rowMapper = determineRowMapper(defaultRowMapper);
